4d510db7608a260dfdfaa05784ebb250d55ffde5
Michael Elkins <elkins@isi.edu>
2025-09-18 20:11:40 -0700

    flush out libhdl.a functions

--- src/hdl.h
+++ src/hdl.h
@@ -18,13 +18,25 @@ typedef struct hdl_cdm_cfg_st {
   int num_iter;
 } hdl_cdm_cfg_t;
 
+typedef struct hdl_cdm_cfg_rsp_st {
+  int rx_bytes;
+} hdl_cdm_cfg_rsp_t;
+
 typedef struct hdl_loop_cfg_st {
+  int is_passive;
 } hdl_loop_cfg_t;
 
+typedef struct hdl_loop_cfg_rsp_st {
+  int rx_bytes;
+} hdl_loop_cfg_rsp_t;
+
 typedef struct hdl_qsdc_cfg_st {
+  int is_passive;
 } hdl_qsdc_cfg_t;
 
 typedef struct hdl_noise_cfg_st {
+  int is_passive;
+  int power;
 } hdl_noise_cfg_t;
 
 // calling code defines this function,
@@ -39,4 +51,18 @@ int hdl_cdm_cfg(hdl_cdm_cfg_t *cfg);
 int hdl_cdm_go(void);
 int hdl_cdm_stop(void);
 
+int hdl_loop_cfg(hdl_loop_cfg_t *cfg, hdl_loop_cfg_rsp_t *);
+int hdl_loop_go(void);
+int hdl_loop_stop(int);
+
+int hdl_qsdc_cfg(hdl_qsdc_cfg_t *cfg);
+int hdl_qsdc_go(void);
+int hdl_qsdc_stop(void);
+
+int hdl_noise_cfg(hdl_noise_cfg_t *cfg);
+int hdl_noise_go(void);
+int hdl_noise_stop(void);
+
+void hdl_errcode_to_errname(int, char **);
+void hdl_get_err_msg(char *, int);
 #endif
